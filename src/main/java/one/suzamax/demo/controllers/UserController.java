dbb71f423b9419f34a79137dd6912635254a042a
Carlos Cañellas Tovar <ratiosu.cct@gmail.com>
2021-06-20 20:22:24 +0200

    Created a REST API of cars and owners

---
+++ src/main/java/one/suzamax/demo/controllers/UserController.java
@@ -0,0 +1,23 @@
+package one.suzamax.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import one.suzamax.demo.repositories.UserRepository;
+
+public class UserController() {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // TODO get all users
+    
+    // TODO get user by id
+    
+    // TODO create user
+
+    // TODO update user
+
+    // TODO delete user
+
+
+}
